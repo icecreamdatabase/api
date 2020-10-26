0fba59ccb283593e518fc338e67c9e3691e72dda
Karl <git@karlneugebauer.com>
2020-10-26 06:46:35 +0100

    access to context

--- src/Gql/resolverMap.ts
+++ src/Gql/resolverMap.ts
@@ -1,5 +1,6 @@
 import {IResolvers} from 'graphql-tools'
 import {PubSub} from 'graphql-subscriptions'
+import {GraphQLResolveInfo} from "graphql"
 
 export class ResolverMap {
   constructor (private _pubsub: PubSub) {
@@ -10,7 +11,7 @@ export class ResolverMap {
       helloWorld (_: void, args: void): string {
         return `👋 Hello world! 👋`
       },
-      test: async (_: any, args: any) => {
+      test: async (parent, args, context, info) => {
         await this._pubsub.publish('commentAdded', {commentAdded: {id: 1, content: 'Hello!'}})
         return `test`
       },
